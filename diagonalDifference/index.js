d521b96806f07ff51d23715bc8b439850981594b
shirin <s.khoramshahi@gmail.com>
2020-03-08 12:04:18 +0330

    solution for diagonalDifference

---
+++ diagonalDifference/index.js
@@ -0,0 +1,19 @@
+function diagonalDifference(arr) {
+    function absolute(value){
+        if(value >=0){
+            return value
+        }
+        return -value
+    }
+    var length = arr.length;
+    var primary_diagonal = 0;
+    var secondary_diagonal = 0;
+    for(var i=0; i< length; i++){
+        primary_diagonal += arr[i][i];
+        secondary_diagonal += arr[i][length-1-i]
+    }
+    var result = primary_diagonal - secondary_diagonal;
+
+    return absolute(result)
+
+}
